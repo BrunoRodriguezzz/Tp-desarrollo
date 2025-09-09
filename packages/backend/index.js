4cadfcceb6135069bad3d1b8ea8ef29d3dbf1008
fmigliavacca <fmigliavacca@frba.utn.edu.ar>
2025-09-09 11:48:07 -0300

    armo las capas de notificacion

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -5,21 +5,23 @@ import Server from "./server/server.js";
 // Importamos los controllers
 import HealthController from "./controllers/healthController.js";
 import ProductoController from "./controllers/productoController.js";
+import NotificacionController from "./controllers/notificacionController.js";
 
 // Importamos los Repositorios
 import ProductoRepository from "./repositories/productoRepository.js";
+import NotificacionRepository from "./repositories/notificacionRepository.js";
 
 // Importamos los servicios
 import ProductoService from "./services/productoService.js";
+import NotificacionService from "./services/notificacionService.js";
 
 import { errorHandler } from "./middlewares/errorHandler.js";
 // Importamos las rutas
 import routes from "./routes/routes.js";
 
-
 // Inicializacion
 const app = express();
-dotenv.config({ path: './packages/backend/.env' });
+dotenv.config({ path: "./packages/backend/.env" });
 
 const port = process.env.SERVER_PORT || 3000;
 //const port = 3000;
@@ -27,17 +29,21 @@ const server = new Server(app, port);
 
 // Capas de Repositorio
 const productoRepository = new ProductoRepository();
+const notificacionRepository = new NotificacionRepository();
 
 // Capas de Servicio
 const productoService = new ProductoService(productoRepository);
+const notificacionService = new NotificacionService(notificacionRepository);
 
 // Capas de Controlador
 const healthController = new HealthController();
 const productoController = new ProductoController(productoService);
+const notificacionController = new NotificacionController();
 
 // Registro de controlladores en el servidor
 server.setController(HealthController, healthController);
 server.setController(ProductoController, productoController);
+server.setController(NotificacionController, notificacionController);
 
 // Configuracion de rutas y lanzamiento
 routes.forEach((route) => server.addRoute(route));
