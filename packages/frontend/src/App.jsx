b35351c34ef21ac16a2530c170634f06b1703cb1
fmigliavacca <fmigliavacca@frba.utn.edu.ar>
2025-10-18 10:24:03 -0300

    pagina de categorias hecha

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./features/layout/Layout";
 import Home from "./features/home/Home";
 import Productos from "./features/productos/Productos";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Categorias from "./features/categorias/Categorias";
 
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="productos" element={<Productos />} />
+            <Route path="categorias" element={<Categorias/>} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ packages/frontend/src/features/categorias/Categorias.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CategoryList from '../../componentes/categories/categoryList/CategoryList'
+import './Categorias.css'
+
+
+export default function Categorias() {
+  return (
+    <div className='categoria-page'>
+      <div className="categoria-content">
+        <h1>Todas las categorías</h1>
+        <p>¡Explora productos por categoría!</p>
+        <CategoryList limit={10} pagination={true} />
+      </div>
+    </div>
+  )
+}
