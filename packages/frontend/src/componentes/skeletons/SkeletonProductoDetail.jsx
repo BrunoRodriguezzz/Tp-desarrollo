21e476f846f0a10d3054698092256c9b5a283fcf
JuanCruzGCH <juancruzgch@gmail.com>
2025-10-25 16:50:09 -0300

    Mini fix en skeleton

--- packages/frontend/src/componentes/skeletons/SkeletonProductoDetail.jsx
+++ packages/frontend/src/componentes/skeletons/SkeletonProductoDetail.jsx
@@ -4,9 +4,7 @@ import Skeleton from '@mui/material/Skeleton';
 export default function ProductDetailSkeleton () {
   return (
     <div className="producto-detail-container">
-      <div className="producto-imagen">
         <Skeleton variant='rectangular' width="85%" height={450} style={{borderRadius: 10}}/>
-      </div>
       <div className="producto-info">
         <Skeleton variant='text' width="20%" height={30}/>
         <Skeleton variant='text' width="70%" height={50}/>
