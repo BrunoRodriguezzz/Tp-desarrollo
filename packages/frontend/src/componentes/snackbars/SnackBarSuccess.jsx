e8f619ceccdbc6b5208903076ef9b0b74997c94d
Barlita03 <nicolas.barlasina18@gmail.com>
2025-10-26 22:13:55 -0300

    Muevo la notificacion de lugar

--- packages/frontend/src/componentes/snackbars/SnackBarSuccess.jsx
+++ packages/frontend/src/componentes/snackbars/SnackBarSuccess.jsx
@@ -1,22 +1,30 @@
+import React from "react";
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
+import PropTypes from "prop-types";
 
-export function SnackbarSuccess({mensaje, open, onClose}) {
+export function SnackbarSuccess({ mensaje, open, onClose }) {
   return (
     <Snackbar
       open={open}
       autoHideDuration={3000}
       onClose={onClose}
-      anchorOrigin={{vertical: "top", horizontal: "right"}}
+      anchorOrigin={{ vertical: "down", horizontal: "right" }}
     >
       <Alert
         onClose={onClose}
         severity="success"
         variant="filled"
-        sx={{width: "100%", padding:"8px 16px", borderRadius: "5px"}}
+        sx={{ width: "100%", padding: "8px 16px", borderRadius: "5px" }}
       >
         {mensaje}
       </Alert>
     </Snackbar>
-  )
+  );
 }
+
+SnackbarSuccess.propTypes = {
+  mensaje: PropTypes.string.isRequired,
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
