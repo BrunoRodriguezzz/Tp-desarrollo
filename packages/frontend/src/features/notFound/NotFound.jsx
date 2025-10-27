3a63907306bb00c0170c6522a25c69eeaee8c808
fmigliavacca <fmigliavacca@frba.utn.edu.ar>
2025-10-27 15:15:59 -0300

    agrego pagina default

---
+++ packages/frontend/src/features/notFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="notfound-container">
+      <h1 className="notfound-title">404</h1>
+      <p className="notfound-text">Página no encontrada</p>
+    </div>
+  );
+}
